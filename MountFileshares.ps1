b4b1a9f3e2d47d8e9697daed527c6d82bea10be5
Joakim Olsson <joakim.olsson@ccornerstone.se>
2021-04-30 17:57:14 +0200

    Initial commit

---
+++ MountFileshares.ps1
@@ -0,0 +1,109 @@
+function Test-Condition{
+    param(
+        [Parameter(Mandatory=$true,  Position=1)][scriptblock]$Test,
+        [Parameter(Mandatory=$false, Position=2)][scriptblock]$OnPass=$null,
+        [Parameter(Mandatory=$false, Position=3)][scriptblock]$OnFail=$null,
+        [Parameter(Mandatory=$false, Position=4)][scriptblock]$Evaluate = { param($v) $true -eq $v }
+    )
+
+    $r = & $Test
+    $pass = & $Evaluate $r
+
+    if ($pass) {
+        if ($OnPass) { & $OnPass }
+    } else {
+        if ($OnFail) { & $OnFail }
+    }
+
+    return $pass
+
+}
+
+function Wait-Condition{
+    param(
+        [Parameter(Mandatory=$true,  Position=1)][scriptblock]$Test,
+        [Parameter(Mandatory=$false, Position=2)][scriptblock]$OnPass=$null,
+        [Parameter(Mandatory=$false, Position=3)][scriptblock]$OnFail=$null,
+        [Parameter(Mandatory=$false, Position=4)][scriptblock]$Evaluate = { param($v) $true -eq $v },
+        [Parameter(Mandatory=$false, Position=5)][int]$IntervalMS=200,
+        [Parameter(Mandatory=$false, Position=6)][int]$TimeoutMS=0
+    )
+
+    $__waitStart__ = [datetime]::Now
+    do {
+        if ($TimeoutMS -gt 0) {
+            $t = ([datetime]::Now - $__waitStart__).TotalMilliSeconds
+            if ($t -gt $TimeOutMS) {
+                if ($OnFail) { & $OnFail }
+                return $false
+            }
+        }
+
+        Start-Sleep -MilliSeconds $IntervalMS
+        $r = Test-Condition -Test $Test -Evaluate $Evaluate
+    } while(!$r)
+
+    if ($OnPass) { & $OnPass }
+
+    return $true
+
+}
+
+$logfile = '{0}/AppData/local/logs/MountFileShares.ps1.log' -f $env:USERPROFILE
+
+Start-Transcript -Path $logfile -Force
+
+$fileServername = "stofs01"
+$fileServerRoot = "\\{0}" -f $fileServername
+
+Wait-Condition { Get-Process explorer -ea SilentlyContinue }
+Wait-Condition { Test-NetConnection $fileServername -Port 445 -InformationLevel Quiet  }
+
+$sharesRaw = net view $fileServerRoot
+$sharesRaw | Write-Host
+
+$sharesArray = $sharesRaw.split("`n")
+$listStart = $sharesArray.indexOf('-'*79) + 1
+$listSize   = $sharesArray.length
+
+for ($i = $listStart; $i -lt $listSize; $i++) {
+	"Line: {0}" -f $i | Write-Host
+	if ($sharesArray[$i] -match "^(?<name>[^\s]+)\s+disk") {
+		$shareName = $matches.name
+		"Share found: {0}" -f $shareName | Write-Host
+		$sharePath = "{0}\{1}" -f $fileServerRoot, $shareName
+		
+		try {
+			
+			try {
+				$driveLetter = cat "$($sharePath)\.mount" -ea Stop
+			} catch {
+				$_ | Write-Host -Foreground Red
+				"Unable to access fileshare."
+				continue
+			}
+			
+			if ($m = Get-SMBMapping -LocalPath "${driveLetter}:" -ea SilentlyContinue) {
+				$m | Remove-SMBMapping -Confirm:$false -UpdateProfile
+			}
+			
+			if ($v = Get-Volume $driveLetter -ea SilentlyContinue) {
+				"Drive letter '{0}' for the share collides with a local volume, skipping." -f $driveLetter | Write-Host -Foreground Yellow
+				continue
+			}
+			
+			if ($d = get-PSDrive $driveLetter -ea SilentlyContinue) {
+				$d | Remove-PSDrive -Scope Global
+			}
+			
+			"Mapping as network drive ({0} -> {1})..." -f $driveLetter, $sharePath | write-Host
+			$drive = New-PSDrive -Name $driveLetter -Root $sharePath -PSProvider FileSystem -Description $shareName -Scope Global -Persist
+			(New-Object -ComObject Shell.Application).NameSpace("$($driveLetter):").Self.Name = $shareName
+			
+		} catch {
+			write-Host $_
+		}
+	}
+}
+
+Stop-Transcript
